259ac655c588f66a0051e3e5c49bda7ca6b46dec
ramit21 <21.ramit@gmail.com>
2021-01-10 23:54:56 +0530

    amplify

---
+++ src/components/DisplayPosts.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class DisplayPosts extends Component {
+
+    render() {
+        return (<div>Hello World</div>)
+    }
+}
+
+export default DisplayPosts;
