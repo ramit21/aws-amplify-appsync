c5fe89ec01f56040eeccda4b47c9845f877cf306
ramit21 <21.ramit@gmail.com>
2021-01-11 00:03:14 +0530

    amplify

--- src/components/DisplayPosts.js
+++ src/components/DisplayPosts.js
@@ -1,6 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { listPosts } from '../graphql/queries'
+import { API, graphqlOperation } from 'aws-amplify'
 
 class DisplayPosts extends Component {
+
+    componentDidMount = async () => {
+        this.getPosts()
+    }
+
+    getPosts = async () => {
+        const result = await API.graphql(graphqlOperation(listPosts));
+        console.log("All posts:", JSON.stringify(result.data.listPosts.items));
+    }
 
     render() {
         return (<div>Hello World</div>)
